d4572c5f460e60f8b16e788f634cdb632cb9649d
Pablo Acuña <pabloacuna88@gmail.com>
2014-01-05 23:10:40 -0300

    remove trash files

--- src/Pacuna/Schemas/Test.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Pacuna\Schemas;
-use Config;
-class Test{
-
-    public function testConfig()
-    {
-        echo Config::get('schemas::var');
-    }
-}
--- src/config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'var' => 'valor',
-];

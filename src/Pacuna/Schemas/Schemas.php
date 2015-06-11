95366de9eb524d941ac7feac16e59355c50c71ff
Andrew Smith <andrew@intellihr.com.au>
2015-06-11 14:47:55 +1000

    Reformat to PSR-2 standard, clean up line endings and add docblocks

--- src/Pacuna/Schemas/Schemas.php
+++ src/Pacuna/Schemas/Schemas.php
@@ -1,12 +1,25 @@
-<?php namespace Pacuna\Schemas;
-/**
- *
- */
+<?php
+
+namespace Pacuna\Schemas;
+
 use DB;
 use Artisan;
 use Schema;
+
+/**
+ * Class Schemas
+ *
+ * @package Pacuna\Schemas
+ */
 class Schemas
 {
+    /**
+     * List all the tables for a schema
+     *
+     * @param string $schemaName
+     *
+     * @return mixed
+     */
     protected function listTables($schemaName)
     {
         $tables = DB::table('information_schema.tables')
@@ -17,16 +30,33 @@ class Schemas
         return $tables;
     }
 
+    /**
+     * Check to see if a table exists within a schema
+     *
+     * @param string $schemaName
+     * @param string $tableName
+     *
+     * @return bool
+     */
     protected function tableExists($schemaName, $tableName)
     {
         $tables = $this->listTables($schemaName);
         foreach ($tables as $table) {
-            if($table->table_name == $tableName) return true;
+            if ($table->table_name === $tableName) {
+                return true;
+            }
         }
 
         return false;
     }
 
+    /**
+     * Check to see if a schema exists
+     *
+     * @param string $schemaName
+     *
+     * @return bool
+     */
     public function schemaExists($schemaName)
     {
         $schema = DB::table('information_schema.schemata')
@@ -37,52 +67,88 @@ class Schemas
         return ($schema > 0);
     }
 
+    /**
+     * Create a new schema
+     *
+     * @param string $schemaName
+     */
     public function create($schemaName)
     {
-        $query = DB::statement('CREATE SCHEMA '.$schemaName);
+        $query = DB::statement('CREATE SCHEMA ' . $schemaName);
     }
 
+    /**
+     * Set the search_path to the schema name
+     *
+     * @param string $schemaName
+     */
     public function switchTo($schemaName = 'public')
     {
-        $query = DB::statement('SET search_path TO '.$schemaName);
+        $query = DB::statement('SET search_path TO ' . $schemaName);
     }
 
+    /**
+     * Drop an existing schema
+     *
+     * @param string $schemaName
+     */
     public function drop($schemaName)
     {
-        $query = DB::statement('DROP SCHEMA '.$schemaName);
+        $query = DB::statement('DROP SCHEMA ' . $schemaName);
     }
 
-    public function migrate($schemaName, $args=[])
+    /**
+     * Run migrations on a schema
+     *
+     * @param string $schemaName
+     * @param array  $args
+     */
+    public function migrate($schemaName, $args = [])
     {
         $this->switchTo($schemaName);
-        if(!$this->tableExists($schemaName, 'migrations'))
-        {
+        if (!$this->tableExists($schemaName, 'migrations')) {
             Artisan::call('migrate:install');
         }
 
         Artisan::call('migrate', $args);
-
     }
 
-    public function migrateRefresh($schemaName, $args=[])
+    /**
+     * Re-run all the migrations on a schema
+     *
+     * @param string $schemaName
+     * @param array  $args
+     */
+    public function migrateRefresh($schemaName, $args = [])
     {
         $this->switchTo($schemaName);
 
         Artisan::call('migrate:refresh', $args);
     }
 
-    public function migrateReset($schemaName, $args=[])
+    /**
+     * Reverse all migrations on a schema
+     *
+     * @param string $schemaName
+     * @param array  $args
+     */
+    public function migrateReset($schemaName, $args = [])
     {
         $this->switchTo($schemaName);
 
         Artisan::call('migrate:reset', $args);
     }
 
-    public function migrateRollback($schemaName, $args=[])
+    /**
+     * Rollback the latest migration on a schema
+     *
+     * @param string $schemaName
+     * @param array  $args
+     */
+    public function migrateRollback($schemaName, $args = [])
     {
         $this->switchTo($schemaName);
 
         Artisan::call('migrate:rollback', $args);
     }
-
 }
--- src/Pacuna/Schemas/SchemasServiceProvider.php
+++ src/Pacuna/Schemas/SchemasServiceProvider.php
@@ -1,37 +1,42 @@
-<?php namespace Pacuna\Schemas;
+<?php
+
+namespace Pacuna\Schemas;
 
 use Illuminate\Support\ServiceProvider;
 
-class SchemasServiceProvider extends ServiceProvider {
+/**
+ * Class SchemasServiceProvider
+ *
+ * @package Pacuna\Schemas
+ */
+class SchemasServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('pgschema', function () {
+            return new Schemas;
+        });
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-	    \App::bind('pgschema', function()
-	    {
-		return new Schemas;
-	    });
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
 }
